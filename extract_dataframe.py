02b4d247fff7b0dc745e0ecaebc7a1567dba6177
theogene <theogene007@gmail.com>
2022-04-27 01:39:47 +0200

    debuged statuses_count method

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -35,8 +35,8 @@ class TweetDfExtractor:
 
     # an example function
     def find_statuses_count(self)->list:
-        statuses_count 
-        
+        statuses_count=[i['user']['statuses_count'] for i in self.tweets_list] 
+        return statuses_count
     def find_full_text(self)->list:
         text = 
        
